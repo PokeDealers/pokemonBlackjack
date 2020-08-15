7a882c7ea924c52bc80ff81bea19f0f858017e84
Stephen McMullin <stephen.mcmullin@icloud.com>
2020-08-15 16:20:02 -0400

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom' 
+import axios from 'axios'
 
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Pokemon blackjack</h1>
-      </div>
-    )
 
-  }
+    componentDidMount() {
+    axios({
+        url: '',
+        responseType: 'json',
+        method: 'GET',
+        params: {
+        // params here
+        }
+    })
+        .then( (res) => {
+            
+        })
+    }
+
+
+    render() {
+        return (
+            <div className="App">
+            <h1>Pokemon blackjack</h1>
+            </div>
+        )
+
+    }
 }
 
 export default App;
