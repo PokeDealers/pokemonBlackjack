ecd753086ddd9be8ce6b7a0168a19fa5a6c01d7b
Ari Charikar <ari.charikar1@gmail.com>
2020-08-16 11:14:57 -0400

    made gh-pages

--- src/App.js
+++ src/App.js
@@ -128,7 +128,7 @@ class App extends Component {
 
     render() {
         return (
-            <Router>
+            <Router basename={process.env.PUBLIC_URL}>
                 <div className="App">
                     <Route exact path="/" component= { Home } />
                     <Route path="/BattleScreen" component={ BattleScreen } />
