235600231efa93198ffcfc51e27c885c20383b09
Ari Charikar <ari.charikar1@gmail.com>
2020-08-16 11:02:52 -0400

    Updates

---
+++ precache-manifest.e27bf0decb64bb23cd76683f49235f8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d64a08a58f5a11a6479f77b42155a2bf",
+    "url": "/PokeDealers/pokemonBlackjack/index.html"
+  },
+  {
+    "revision": "e37fcfe96e8836fd3d22",
+    "url": "/PokeDealers/pokemonBlackjack/static/css/main.b422d4ad.chunk.css"
+  },
+  {
+    "revision": "d63b7b54fa5d5beda908",
+    "url": "/PokeDealers/pokemonBlackjack/static/js/2.038a1494.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/PokeDealers/pokemonBlackjack/static/js/2.038a1494.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e37fcfe96e8836fd3d22",
+    "url": "/PokeDealers/pokemonBlackjack/static/js/main.25f70322.chunk.js"
+  },
+  {
+    "revision": "4e3fa7340f8d8754524c",
+    "url": "/PokeDealers/pokemonBlackjack/static/js/runtime-main.8fce3537.js"
+  }
+]);
